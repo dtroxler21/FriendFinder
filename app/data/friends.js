aaff6a43bd4f5d7b56061b763140f286e984941a
dtroxler21 <dtroxler21@gmail.com>
2018-04-20 12:24:37 -0400

    committing everything done so far

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var allFriends = [
+    {
+      name: "Jimmy Fallon",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/9/9e/Jimmy_Fallon%2C_Montclair_Film_Festival%2C_2013.jpg",
+      scores: ["3","4","2","1","5","3","4","5","3","5"],
+    },
+    {
+      name: "Kristen Bell",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/9/93/Kristen_Bell_Frozen_premiere_2013_%28cropped%29.jpg",
+      scores: ["3","3","4","4","5","5","5","5","5","5"]
+    },
+    {
+      name: "Donald Trump",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg",
+      scores: ["1","4","1","1","1","5","5","1","1","1"]
+    }
+  ];
+
+  module.exports = allFriends;
